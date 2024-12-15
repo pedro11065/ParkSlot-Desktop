11eb5c696728fb68182570664101b3a19ff51f02
pedro11065 <pedrohenriquesilvaquixabeira@gmail.com>
2024-12-15 14:05:03 -0300

    table "table_user" created

--- test/sql.sql
+++ test/sql.sql
@@ -1,15 +1,10 @@
-create table parkslot_now
-(id uuid primary key,
-plate varchar(7) unique,
-custumer_name varchar(225),
-entry_time time,
-entry_date date);
-
-create table parkslot_historic
-(id uuid primary key,
-plate varchar(7) unique,
-custumer_name varchar(225),
-entry_time time,
-entry_date date,
-exit_time time,
-exit_date date);
+CREATE TABLE table_user(
+user_id UUID primary key,
+fullname varchar(40),
+email varchar(50),
+birthdate date,
+password varchar(150),
+terms bool,
+creation_date date,
+creation_time time
+);
